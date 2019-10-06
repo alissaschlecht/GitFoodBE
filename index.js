2d94dc76e91bd7ab1b42a221ecf73024108f1617
Alissa Schlecht <alissaschlecht@Cathleens-MacBook-Pro.local>
2019-10-06 19:44:20 -0400

    update app port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 // Safety first: listen only on first import to avoid EADDRINUSE exceptions
 if (!module.parent) {
-  app.listen(8080, () => {
-    console.log(`GitFood backend listening on port 8080`);
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`GitFood backend listening on port ${process.env.PORT || 8080}`);
   });
 }
