13c45e2f9de4cbb3722fce532ec358b63fdd116e
Alissa Schlecht <alissaschlecht@Cathleens-MacBook-Pro.local>
2019-10-08 15:55:25 -0400

    add cors for express

--- app.js
+++ app.js
@@ -34,30 +34,9 @@ umzug.up()
 
 // create express app
 const app = express();
-// app.use(cors());
-
-var allowedOrigins = ['http://localhost:3000'];
-app.use(cors({
-
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-
-  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
-
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
-
 
 
 module.exports = app;
